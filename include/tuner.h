2627071f05d0cb9aa0149cc571a715bbff03d0c5
sampozz <samuele.pozzani@gmail.com>
2022-12-10 16:09:29 +0100

    revert

--- include/tuner.h
+++ include/tuner.h
@@ -1,28 +1,12 @@
 #ifndef _TUNER_H_
 #define _TUNER_H_
 
-#include <ti/devices/msp432p4xx/inc/msp.h>
 
-void dsp_init(void);
+double note_pitch(int note_index);
 
-double pitch_detection(int16_t (*data_array)[]);
-
-/**
- * Return the exact pitch of the note idetified by note_number,
- * where A4 = 0, A# = 1, G# = -1
- */
-double note_pitch(int note_number);
-
-/**
- * Return the index of the nearest note to pitch
- */
 int note_index(double pitch);
 
-/**
- * Copy the name of the nearest note to pitch into name buffer
- */
 void note_name(double pitch, char name[]);
 
-void note_pitch_range(double pitch, double *max_pitch, double *min_pitch);
 
 #endif /* _TUNER_H_ */
--- src/tuner.c
+++ src/tuner.c
@@ -6,44 +6,17 @@
 float hann[SAMPLE_LENGTH];
 const char notes[12][3] = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
 
-void dsp_init(void)
+double note_pitch(int note_index)
 {
-    // Initialize Hann Window
-    int n;
-    for (n = 0; n < SAMPLE_LENGTH; n++)
-        hann[n] = 0.5f - 0.5f * cosf((2 * M_PI * n) / (SAMPLE_LENGTH - 1));
-}
-
-double pitch_detection(int16_t (*data_array)[])
-{
-    /* Apply Hann window to signal */
-    int i;
-    for (i = 0; i < SAMPLE_LENGTH; i++)
-        (*data_array)[i] = (int16_t) (hann[i] * (*data_array)[i]);
-
-    /* Compute yin algorithm */
-    Yin yin;
-    Yin_init(&yin, SAMPLE_LENGTH, SAMPLE_FREQUENCY, 0.05);
-    return Yin_getPitch(&yin, *data_array);
-}
-
-double note_pitch(int note_number)
-{
-    return pow(2, (double)note_number / 12.0) * 440.0;
-}
-
-int note_number(double pitch)
-{
-    double index = 12.0 * log2(pitch / 440.0);
-    if (index < 0)
-        return index - 0.5;
-    return index + 0.5;
+    return pow(2, (double)note_index / 12.0) * 440.0;
 }
 
 int note_index(double pitch)
 {
     double index = 12.0 * log2(pitch / 440.0);
+
     int int_index = index + 120.5;
+
     return int_index % 12;
 }
 
@@ -51,16 +24,4 @@ void note_name(double pitch, char name[])
 {
     int i = note_index(pitch);
     strcpy(name, notes[i]);
-}
-
-void note_pitch_range(double pitch, double *max_pitch, double *min_pitch)
-{
-    int note = note_number(pitch);
-    double correct_pitch = note_pitch(note);
-
-    int prev_note = note - 1;
-    int next_note = note + 1;
-
-    *min_pitch = (note_pitch(prev_note) + correct_pitch) / 2.0;
-    *max_pitch = (note_pitch(next_note) + correct_pitch) / 2.0;
 }
